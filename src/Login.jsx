4b3fac8fbb6bd4bfa2b08f6a7088218f88e6f207
vishnukanthnvv <vishnukanth.n@gmail.com>
2026-02-15 00:37:31 +0530

    create login page add Axios library for API calls and necessary cofig for CORS issues

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+
 
 const Login = () => {
-  return (
-    <div>Login Page</div>
-  )
-}
+    const [emailId, updateEmailId] = useState("");
+    const [password, updatePassword] = useState("");
 
-export default Login
+    const handleLogin = async () => {
+      try {
+        const res = await axios.post(
+          "http://localhost:3000/login",
+          {
+            emailId,
+            password,
+          },
+          {
+            withCredentials: true,
+          },
+        );
+        console.log(res);
+      } catch (err) {
+        console.log(err.nessage);
+      }
+    };
+
+    return (
+        <div className="flex justify-center my-10">
+        <div className="card w-96 bg-base-300 card-xl shadow-sm">
+            <div className="card-body flex justify-center">
+            <h2 className="card-title justify-center">Login</h2>
+            <div className="email my-2">
+                <label className="input validator">
+                <svg
+                    className="h-[1em] opacity-50"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                >
+                    <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                    >
+                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                    </g>
+                </svg>
+                <input type="email" value={emailId} onChange={(e) => updateEmailId(e.target.value)} placeholder="mail@site.com" required />
+                </label>
+                <div className="validator-hint hidden">
+                Enter valid email address
+                </div>
+                <div className="justify-end card-actions"></div>
+            </div>
+            <div className="password" my-2>
+                <label className="input validator">
+                <svg
+                    className="h-[1em] opacity-50"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                >
+                    <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                    >
+                    <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                    <circle
+                        cx="16.5"
+                        cy="7.5"
+                        r=".5"
+                        fill="currentColor"
+                    ></circle>
+                    </g>
+                </svg>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => updatePassword(e.target.value)}
+                    required
+                    placeholder="Password"
+                    minLength="8"
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                />
+                </label>
+                <p className="validator-hint hidden">
+                Must be more than 8 characters, including <br />
+                At least one number <br />
+                At least one lowercase letter <br />
+                At least one uppercase letter
+                </p>
+            </div>
+            <button className="btn btn-primary my-5" onClick={handleLogin}>Login</button>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Login;
